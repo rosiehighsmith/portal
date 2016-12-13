c0519bc2d244a534729bbffb2b4b04b066c1db5f
Rosie <ariel.highsmith@gmail.com>
2016-12-13 14:01:36 -0800

    Implement supervision tree for r for recovery

--- lib/portal.ex
+++ lib/portal.ex
@@ -6,15 +6,11 @@ defmodule Portal do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: Portal.Worker.start_link(arg1, arg2, arg3)
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
+      worker(Portal.Door, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
   defstruct [:left, :right]
@@ -45,5 +41,31 @@ defmodule Portal do
 
     # Let's return the portal itself
     portal
+  end
+
+  @doc """
+  Shoots a new door with the given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
+
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
   end
 end
